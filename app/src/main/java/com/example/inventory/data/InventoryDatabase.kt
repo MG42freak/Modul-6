e7e94e0e6b2676428dd0496adfbdedfdb11d08be
MG42freak <mg42freak@gmail.com>
2025-06-08 02:34:35 +0700

    Create a Database instance

--- app/src/main/java/com/example/inventory/data/InventoryDatabase.kt
+++ app/src/main/java/com/example/inventory/data/InventoryDatabase.kt
@@ -37,12 +37,6 @@ abstract class InventoryDatabase : RoomDatabase() {
             // if the Instance is not null, return it, otherwise create a new database instance.
             return Instance ?: synchronized(this) {
                 Room.databaseBuilder(context, InventoryDatabase::class.java, "item_database")
-                    /**
-                     * Setting this option in your app's database builder means that Room
-                     * permanently deletes all data from the tables in your database when it
-                     * attempts to perform a migration with no defined migration path.
-                     */
-                    .fallbackToDestructiveMigration()
                     .build()
                     .also { Instance = it }
             }
